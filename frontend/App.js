7cac7e121021c10b14caebacbfb65b3fcc18af89
Adam Ding <wqding@uwaterloo.ca>
2020-04-30 13:00:41 -0600

    did some things

--- frontend/App.js
+++ frontend/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, StyleSheet } from 'react-native';
+import Home from './components/Home'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style = {styles.container}>
+      <Home/>
     </View>
   );
 }
@@ -12,7 +13,6 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
