ba65f0604071d7858ef8f7aa4ee79876df9f60b4
Adam Ding <wqding@uwaterloo.ca>
2020-05-03 19:47:57 -0600

    testing recording component

--- frontend/App.js
+++ frontend/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { View, StyleSheet } from 'react-native';
 import HomePage from './components/HomePage'
 import RecordingsPage from './components/RecordingsPage'
+import TestPage from './components/TestPage'
 import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
@@ -12,9 +13,8 @@ export default function App() {
   return (
     <NavigationContainer>
         <Stack.Navigator>
-          <Stack.Screen
-            name="HomePage" component={HomePage} 
-          />
+          <Stack.Screen name="TestPage" component={TestPage} />
+          <Stack.Screen name="HomePage" component={HomePage} />
           <Stack.Screen name="RecordingsPage" component={RecordingsPage} options={{title: 'Recordings'}}/>
         </Stack.Navigator>
       </NavigationContainer>
