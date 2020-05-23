4af69d89f596a900610cb2cef8994d8ca69abf2d
Adam Ding <wqding@uwaterloo.ca>
2020-05-23 12:37:18 -0600

    proper file streaming to websocket

--- frontend/src/components/Landing.js
+++ frontend/src/components/Landing.js
@@ -7,37 +7,36 @@ const Landing = () => {
   const onDrop = useCallback((acceptedFiles) => {
 
     acceptedFiles.forEach((file) => {
-      const reader = new FileReader();
+      var chunkSize = 5 * 1024 * 1024;
+      var fileSize = file.size;
+      var numChunks = Math.ceil(fileSize/chunkSize,chunkSize);
+      var chunkIdx = 0;
+    
+      console.log('file size..',fileSize);
+      console.log('chunks...',numChunks);
+    
+      while (chunkIdx <= numChunks) {
+        const reader = new FileReader();
 
-      reader.onabort = () => console.log('file reading was aborted')
-      reader.onerror = () => console.log('file reading has failed')
+        reader.onabort = () => console.log('file reading was aborted')
+        reader.onerror = () => console.log('file reading has failed')
 
-      reader.onload = () => {
-        const arrayBuffer = new Uint8Array(reader.result);
+        reader.onload = () => {
+          const arrayBuffer = new Uint8Array(reader.result);
 
-        console.log(arrayBuffer)
+          console.log(arrayBuffer)
+          WebSocketInstance.send(arrayBuffer)
+        }
 
-        //3mb
-        var chunkSize = 5 * 1024 * 1024;
-        var fileSize = arrayBuffer.length;
-        var numChunks = Math.ceil(fileSize/chunkSize,chunkSize);
-        var chunkIdx = 1;
-      
-        console.log('file size..',fileSize);
-        console.log('chunks...',numChunks);
-      
-        // while (chunkIdx <= numChunks) {
-            var offset = chunkIdx*chunkSize;
-            console.log('current chunk..', chunkIdx);
-            console.log('offset...', chunkIdx*chunkSize);
-            console.log('file blob from offset...', offset)
-            var chunk = arrayBuffer.slice(offset,offset+chunkSize)
-            console.log(chunk);
-            chunkIdx++;
-            WebSocketInstance.send(chunk)
-        // }
+        var offset = chunkIdx*chunkSize;
+        console.log('current chunk..', chunkIdx);
+        console.log('offset...', chunkIdx*chunkSize);
+        console.log('file blob from offset...', offset)
+        var chunk = file.slice(offset,offset+chunkSize)
+        console.log(chunk);
+        chunkIdx++;
+        reader.readAsArrayBuffer(chunk)
       }
-      reader.readAsArrayBuffer(file)
       
     })
     
